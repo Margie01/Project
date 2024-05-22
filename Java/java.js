a39d21d98e1b28e27c422a36cc5ea31aee3efb0f
Margaretha Wilson <margaretha.pekis@gmx.de>
2024-05-22 13:06:18 +0930

    java init

---
+++ Java/java.js
@@ -0,0 +1,9 @@
+let fullName = "Margie"
+let age = 33
+let student = true
+
+document.getElementById('p1').textContent = 'your name is ${fullName}';
+document.getElementById('p2').textContent = 'your age is ${age}';
+document.getElementById('p3').textContent = 'enrolled at uni ${student}';
+
+
